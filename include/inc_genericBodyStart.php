7dd1358350beceea9ed5b52b887dd217ac44c9ec
wwood <44790300+warrenwood@users.noreply.github.com>
2022-10-14 13:50:52 -0700

    added two include files

---
+++ include/inc_genericBodyStart.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>CPT App Challenge - Null Pointers: Services</title>
+		<meta name="viewport" content="width=device-width,initial-scale=1">
+	</head>
+	<body>
