0f748d42ffc3d8bdfcc344c480d01751412c5541
wwood <44790300+warrenwood@users.noreply.github.com>
2022-10-18 14:57:49 -0700

    reorganize client/server/common

--- public/client.php
+++ public/client.php
@@ -2,6 +2,7 @@
 	
 	require_once($_SERVER['DOCUMENT_ROOT'].'/../include/inc_dbfunctions.php');
 	$link = dbConnect();
+	$http_root_dir = 'https://'.$_SERVER['HTTP_HOST'].'/';
 ?>
 
 
@@ -9,14 +10,19 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
 <html lang="en">
 	<head>
+	
 		<meta charset="UTF-8">
-		<title>CPT App Challenge - Null Pointers: Team | Warren</title>
+		<title>CPT App Challenge - Null Pointers</title>
 		<meta name="viewport" content="width=device-width,initial-scale=1">
-		<link rel="stylesheet" href="warren.css">
-		<script src="warren.js" ></script>
+		<script type="text/javascript" src="<?php echo $http_root_dir ?>common/js/common.js"></script>
+		<link rel="stylesheet" href="<?php echo $http_root_dir ?>common/css/common.css">
 		<script type='text/javascript'>
-
-
+		</script>
+	</head>
+	<body>
+		test client. <span class="hidden">hidden span</span>
+	</body>
+</html>
 
 <?php
 	dbDisconnect($link);
