7210680a09c4913c04289ccbb9bebb1616553781
wwood <44790300+warrenwood@users.noreply.github.com>
2022-10-19 11:36:13 -0700

    Update client.php

--- public/client.php
+++ public/client.php
@@ -16,7 +16,7 @@
 	<head>
 	
 		<meta charset="UTF-8">
-		<title>CPT App Challenge - Null Pointers</title>
+		<title>CPT App Challenge - Null Pointers - client v1.1</title>
 		<meta name="viewport" content="width=device-width,initial-scale=1">
 		<script type="text/javascript" src="/common/js/common.js"></script>
 		<link rel="stylesheet" href="/common/css/common.css">
